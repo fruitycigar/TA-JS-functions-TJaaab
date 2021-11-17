f67d647193c303d85fafe45963796a3b73dacd60
fruitycigar <shruthisagar@SHODAN.local>
2021-11-17 17:29:43 +0530

    Assignment: Arguments and Returns (Block 4) updated

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -32,13 +32,12 @@ addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
 
 function addTwoNumbers(firstNum, secondNum) {
-  if (typeof(firstNum) !== "Number" || typeof(secondNum) !== "Number") {
-    alert(`Enter valid input.`);
-  }
-  return firstNum + secondNum;
+ if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+   alert(`Please enter a valid number.`);
+ } else {
+   return firstNum + secondNum;
+ }
 }
-
-addTwoNumbers();
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -52,7 +51,23 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc(numA, numB, operation) {
 
+  if (typeof numA !== "number" || typeof numB !== "number") {
+    alert(`Don't fool around. Enter a valid number before I call the cops on you.`);
+  } else {
+    switch (operation) {
+      case "add":
+        return numA + numB;
+      case "sub":
+        return numA - numB;
+      case "mul":
+        return numA * numB;
+      case "div":
+        return numA / numB;
+    }
+  }
+}
 
 
 /*
@@ -62,12 +77,19 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
 function isLeapYear(year) {
-
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  return year % 4 === 0;
 }
 
-
 /*
-6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
-*/
+6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.*/
+
+function getFactorial(number) {
+  let final = 1
+  for (let i = number; i >= 1; i--) {
+    final *= i;
+  }
+  return final;
+}
