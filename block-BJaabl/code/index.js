66364fc90f276e548d9e74693102d47cccd06649
fruitycigar <shruthisagar@SHODAN.local>
2021-11-17 16:31:51 +0530

    Assignment: Arguments and Return incomplete

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,10 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+function sayHello(name) {
+  alert(`Hello ${name}.`)
+}
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +14,12 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}.`;
+}
+
+getFullName();
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -20,6 +30,15 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+
+function addTwoNumbers(firstNum, secondNum) {
+  if (typeof(firstNum) !== "Number" || typeof(secondNum) !== "Number") {
+    alert(`Enter valid input.`);
+  }
+  return firstNum + secondNum;
+}
+
+addTwoNumbers();
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -34,6 +53,8 @@ calc(20, 10, 'mul'); // 200
 */
 
 
+
+
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
 on if the year id leap year or not.
@@ -41,6 +62,10 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
+
+function isLeapYear(year) {
+
+}
 
 
 /*
